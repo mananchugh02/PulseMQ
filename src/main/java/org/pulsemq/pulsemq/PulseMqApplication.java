6acfd9e04ee1c251becf2d8198e91c9bc3f28e69
mananchugh02 <manan21335@iiitd.ac.in>
2026-05-10 14:43:10 +0530

    Initial commit

---
+++ src/main/java/org/pulsemq/pulsemq/PulseMqApplication.java
@@ -0,0 +1,13 @@
+package org.pulsemq.pulsemq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulseMqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PulseMqApplication.class, args);
+    }
+
+}
